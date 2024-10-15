3199afa4959bc56a60080f4435fee3ddd4207fdb
sparesolutions <sparesolutions2@gmail.com>
2024-10-15 11:38:49 -0700

    Add files via upload

---
+++ Spare-s&cars/script.js
@@ -0,0 +1,21 @@
+// (function showSidebar(){
+//     const sidebar = document.querySelector('.sidebar')
+//     sidebar.stlye.display = 'flex'
+// }
+// function hideSidebar(){
+//     const sidebar = document.querySelector('.sidebar')
+//     sidebar.addEventListener('click')
+// }
+// const navBar = document.querySelector("nav"),
+// menuBtns = addEventListener("nav img");
+// // overlay = document.querySelector("overlay");
+
+// menuBtns.forEach((menuBtns) => {
+// menuBtns.addEventListener("click" () => {
+//     navBar.classList.toggle('open');
+// })
+// });
+
+// overlay.addEventListener("click", () => {
+// navBar.classList.remove("open")
+// });
